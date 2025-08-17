7bb91e4e00eeec61b93036bc13260ea2ed47050c
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-17 18:05:41 +0530

    route groups add

--- nextjs-app/app/(users)/layout.jsx
+++ nextjs-app/app/(users)/layout.jsx
@@ -1,11 +1,11 @@
-import Navigation from "./components/Navigation";
-import "./globals.css";
-
+import Navigation from "@/components/Navigation"
+import "../globals.css";
+   
 export default function RootLayout({children}){
   return (
     <html>
       <body>
-      <h1>Hello from RootLayout</h1>
+      {/* <h1>Hello from RootLayout</h1> */}
       <Navigation />
         {children}
       </body>
